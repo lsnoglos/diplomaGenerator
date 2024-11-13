c74fdeb074abc19426b18262d95f43a8048b2ab0
noellopez <noel.lopez@mqaamericas.com>
2024-11-13 10:59:45 -0600

    add: select font button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [fontPath, setFontPath] = useState('');
+
+  const selectFont = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setFontPath(file);
+    }
+  };
+
   return (
     <div>
       <h1>Diploma Generator</h1>
+
+      <div>
+        <button>
+          <label>
+            Seleccione letra
+            <input type="file" accept=".ttf" onChange={selectFont} style={{ display: 'none' }} />
+          </label>
+        </button>
+        <span>{fontPath.name || 'Seleccione tipo de letra'}</span>
+      </div>
     </div>
   );
 }
