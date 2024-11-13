513f34d98e7fcc50681f5730873932390514eab8
noellopez <noel.lopez@mqaamericas.com>
2024-11-13 11:01:38 -0600

    add: select background image

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React, { useState } from 'react';
 
 function App() {
   const [fontPath, setFontPath] = useState('');
+  const [imgPath, setImgPath] = useState('');
 
   const selectFont = (e) => {
     const file = e.target.files[0];
     if (file) {
       setFontPath(file);
+    }
+  };
+
+  const selectBackgroundImage = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImgPath(URL.createObjectURL(file));
     }
   };
 
@@ -23,6 +31,17 @@ function App() {
         </button>
         <span>{fontPath.name || 'Seleccione tipo de letra'}</span>
       </div>
+
+      <div>
+        <button>
+          <label>
+            Seleccione Imagen
+            <input type="file" accept="image/*" onChange={selectBackgroundImage} style={{ display: 'none' }} />
+          </label>
+        </button>
+        <span>{imgPath ? 'Imagen seleccionada' : 'Seleccione imagen de fondo'}</span>
+      </div>
+
     </div>
   );
 }
